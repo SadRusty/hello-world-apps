bb85b13714cb189db20c46cb42af4143d29cfcf4
SadRusty <47248329+SadRusty@users.noreply.github.com>
2020-11-18 15:40:54 +0500

    Updates

--- precache-manifest.1d59a50f0f8403f686cc8174a8485196.js
+++ precache-manifest.1d59a50f0f8403f686cc8174a8485196.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e2c985dd19bf6897608af4a28e5f382",
+    "revision": "086cb73c7c698c412c1c6e84cb32edeb",
     "url": "./index.html"
   },
   {
-    "revision": "62670e39f66ed86047da",
+    "revision": "ee12a96e1e3bcef48063",
     "url": "./static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "8f51794f2ce96387013b",
+    "revision": "52ed038e65a342f77bba",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "62670e39f66ed86047da",
-    "url": "./static/js/2.0b90d655.chunk.js"
+    "revision": "ee12a96e1e3bcef48063",
+    "url": "./static/js/2.bf4fb9c4.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.0b90d655.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.bf4fb9c4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8f51794f2ce96387013b",
-    "url": "./static/js/main.498d10c7.chunk.js"
+    "revision": "52ed038e65a342f77bba",
+    "url": "./static/js/main.c477833c.chunk.js"
   },
   {
     "revision": "fb54b6bed906a3e09b6a",
